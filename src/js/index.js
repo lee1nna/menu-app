1ea5981c30cbb36096bfe7d751758e2085687e4e
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2022-04-20 22:42:30 +0900

    메뉴 수정 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -11,8 +11,8 @@ const $ = (selector) => document.querySelector(selector);
 
 function App() {
     // TODO 메뉴 수정
-    // - [ ] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
-    // - [ ] 모달창에서 신규메뉴명을 입력받고, 확인버튼을 누르면 메뉴가 수정된다.
+    // - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
+    // - [x] 모달창에서 신규메뉴명을 입력받고, 확인버튼을 누르면 메뉴가 수정된다.
 
     $("#espresso-menu-list").addEventListener("click", (e) => {
         if(e.target.classList.contains('menu-edit-button')) {
